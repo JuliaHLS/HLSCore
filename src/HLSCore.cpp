d2dc85a7616d9281a9cf13668616501b4900d6ce
ben <BenShort>
2024-12-27 23:35:01 +0000

    reduced core

--- src/HLSCore.cpp
+++ src/HLSCore.cpp
@@ -41,12 +41,8 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/ToolOutputFile.h"
 
-#include "circt/Conversion/CalyxToFSM.h"
 #include "circt/Conversion/ExportVerilog.h"
 #include "circt/Conversion/Passes.h"
-#include "circt/Conversion/SCFToCalyx.h"
-#include "circt/Dialect/Calyx/CalyxDialect.h"
-#include "circt/Dialect/Calyx/CalyxPasses.h"
 #include "circt/Dialect/DC/DCPasses.h"
 #include "circt/Dialect/ESI/ESIDialect.h"
 #include "circt/Dialect/ESI/ESIPasses.h"
@@ -63,79 +59,8 @@ using namespace llvm;
 using namespace mlir;
 using namespace circt;
 
-// --------------------------------------------------------------------------
-// Tool options
-// --------------------------------------------------------------------------
-
-static cl::OptionCategory mainCategory("hlstool Options");
-
-static cl::opt<std::string> inputFilename(cl::Positional,
-                                          cl::desc("<input file>"),
-                                          cl::init("-"), cl::cat(mainCategory));
-
-static cl::opt<std::string> outputFilename(
-    "o", cl::desc("Output filename, or directory for split output"),
-    cl::value_desc("filename"), cl::init("-"), cl::cat(mainCategory));
-
-static cl::opt<bool>
-    splitInputFile("split-input-file",
-                   cl::desc("Split the input file into pieces and process each "
-                            "chunk independently"),
-                   cl::init(false), cl::Hidden, cl::cat(mainCategory));
-
-static cl::opt<bool>
-    verifyDiagnostics("verify-diagnostics",
-                      cl::desc("Check that emitted diagnostics match "
-                               "expected-* lines on the corresponding line"),
-                      cl::init(false), cl::Hidden, cl::cat(mainCategory));
-
-static cl::opt<bool>
-    verbosePassExecutions("verbose-pass-executions",
-                          cl::desc("Log executions of toplevel module passes"),
-                          cl::init(false), cl::cat(mainCategory));
-
-static cl::opt<bool>
-    verifyPasses("verify-each",
-                 cl::desc("Run the verifier after each transformation pass"),
-                 cl::init(true), cl::cat(mainCategory));
-
-static cl::opt<bool>
-    allowUnregisteredDialects("allow-unregistered-dialects",
-                              cl::desc("Allow unknown dialects in the input"),
-                              cl::init(false), cl::Hidden,
-                              cl::cat(mainCategory));
-
-enum HLSFlow {
-  // Compilation through the dynamically scheduled handshake dialect.
-  HLSFlowDynamicHW,
-  // Compilation through Calyx's CIRCT lowering implementation.
-  HLSFlowCalyxHW,
-};
-
-static cl::opt<HLSFlow>
-    hlsFlow(cl::desc("HLS flow"),
-            cl::values(clEnumValN(HLSFlowDynamicHW, "dynamic-hw",
-                                  "Dynamically scheduled (HW path)"),
-                       clEnumValN(HLSFlowCalyxHW, "calyx-hw",
-                                  "Statically scheduled (Calyx path)")),
-            cl::cat(mainCategory));
-
 enum DynamicParallelismKind { None, Locking, Pipelining };
-
-static cl::opt<DynamicParallelismKind> dynParallelism(
-    "dynamic-parallelism", cl::desc("Specify the DHLS task parallelism kind"),
-    cl::values(
-        clEnumValN(None, "none",
-                   "Add no protection mechanisms that could prevent data races "
-                   "when a function has multiple active invocations."),
-        clEnumValN(Locking, "locking",
-                   "Add function locking protection mechanism which ensures "
-                   "that only one function invocation is active."),
-        clEnumValN(Pipelining, "pipelining",
-                   "Add function pipelining mechanism that enables a "
-                   "pipelined execution of multiple function invocations while "
-                   "preserving correctness.")),
-    cl::init(Pipelining), cl::cat(mainCategory));
+enum OutputFormatKind { OutputIR, OutputVerilog, OutputSplitVerilog };
 
 enum IRLevel {
   // A high-level dialect like affine or scf
@@ -152,82 +77,21 @@ enum IRLevel {
   SV
 };
 
-static cl::opt<IRLevel> irInputLevel(
-    "input-level",
-    cl::desc("Level at which to input IR at. It is flow-defined "
-             "which value corersponds to which IR level."),
-    cl::values(
-        clEnumValN(High, "high", "High-level dialects like affine"),
-        clEnumValN(
-            PreCompile, "pre-compile",
-            "The IR right before the core lowering dialect (handshake/calyx)"),
-        clEnumValN(Core, "core", "The IR in core dialect (handshake/calyx)"),
-        clEnumValN(
-            PostCompile, "post-compile",
-            "The lowest form of core IR (handshake/calyx) (i.e. after all "
-            "passes have run)"),
-        clEnumValN(RTL, "rtl", "The IR after lowering is performed")),
-    cl::init(IRLevel::High), cl::cat(mainCategory));
+auto dynParallelism = Pipelining;
+bool withESI = false;
+std::string bufferingStrategy = "all";
+unsigned bufferSize = 2;
+IRLevel irInputLevel = High;
+IRLevel irOutputLevel = SV;
+bool splitInputFile = false;
 
-static cl::opt<IRLevel> irOutputLevel(
-    "output-level",
-    cl::desc("Level at which to output IR at. It is flow-defined "
-             "which value corersponds to which IR level."),
-    cl::values(
-        clEnumValN(
-            PreCompile, "pre-compile",
-            "The IR right before the core lowering dialect (handshake/calyx)"),
-        clEnumValN(Core, "core", "The IR in core dialect (handshake/calyx)"),
-        clEnumValN(
-            PostCompile, "post-compile",
-            "The lowest form of core IR (handshake/calyx) (i.e. after all "
-            "passes have run)"),
-        clEnumValN(RTL, "rtl", "The IR after lowering is performed"),
-        clEnumValN(SV, "sv", "System verilog representation")),
-    cl::init(IRLevel::SV), cl::cat(mainCategory));
-
-enum OutputFormatKind { OutputIR, OutputVerilog, OutputSplitVerilog };
-
-static cl::opt<OutputFormatKind> outputFormat(
-    cl::desc("Specify output format:"),
-    cl::values(clEnumValN(OutputIR, "ir", "Emit post-HLS IR"),
-               clEnumValN(OutputVerilog, "verilog", "Emit Verilog"),
-               clEnumValN(OutputSplitVerilog, "split-verilog",
-                          "Emit Verilog (one file per module; specify "
-                          "directory with -o=<dir>)")),
-    cl::init(OutputVerilog), cl::cat(mainCategory));
-
-static cl::opt<bool>
-    traceIVerilog("sv-trace-iverilog",
-                  cl::desc("Add tracing to an iverilog simulated module"),
-                  cl::init(false), cl::cat(mainCategory));
-
-// --------------------------------------------------------------------------
-// Handshake options
-// --------------------------------------------------------------------------
-
-static cl::opt<std::string>
-    bufferingStrategy("buffering-strategy",
-                      cl::desc("Strategy to apply. Possible values are: "
-                               "cycles, allFIFO, all (default)"),
-                      cl::init("all"), cl::cat(mainCategory));
-
-static cl::opt<unsigned> bufferSize("buffer-size",
-                                    cl::desc("Number of slots in each buffer"),
-                                    cl::init(2), cl::cat(mainCategory));
-
-static cl::opt<bool> withESI("with-esi",
-                             cl::desc("Create ESI compatible modules"),
-                             cl::init(false), cl::cat(mainCategory));
-
-static cl::opt<bool> withDC("dc", cl::desc("Use the DC flow"), cl::init(false),
-                            cl::cat(mainCategory));
-
-static LoweringOptionsOption loweringOptions(mainCategory);
-
-// --------------------------------------------------------------------------
-// (Configurable) pass pipelines
-// --------------------------------------------------------------------------
+OutputFormatKind outputFormat = OutputVerilog;
+bool traceIVerilog = false;
+bool withDC = false;
+bool verifyPasses = true;
+std::string inputFilename = "/home/ben/HLSCore/playground/example.mlir";
+std::string outputFilename = "-";
+bool verifyDiagnostics = false;
 
 /// Create a simple canonicalizer pass.
 static std::unique_ptr<Pass> createSimpleCanonicalizerPass() {
@@ -366,8 +230,8 @@ static LogicalResult doHLSFlowDynamic(
   if (traceIVerilog)
     pm.addPass(circt::sv::createSVTraceIVerilogPass());
 
-  if (loweringOptions.getNumOccurrences())
-    loweringOptions.setAsAttribute(module);
+  /*if (loweringOptions.getNumOccurrences())*/
+  /*  loweringOptions.setAsAttribute(module);*/
   if (outputFormat == OutputVerilog) {
     pm.addPass(createExportVerilogPass((*outputFile)->os()));
   } else if (outputFormat == OutputSplitVerilog) {
@@ -384,91 +248,6 @@ static LogicalResult doHLSFlowDynamic(
   return success();
 }
 
-static LogicalResult doHLSFlowCalyx(
-    PassManager &pm, ModuleOp module,
-    std::optional<std::unique_ptr<llvm::ToolOutputFile>> &outputFile) {
-
-  // XXX(rachitnigam): Duplicated from doHLSFlowDynamic. We should probably
-  // abstract this pattern. The only problem is that addIRLevel captures this
-  // mutable variable.
-  bool suppressLaterPasses = false;
-  auto notSuppressed = [&]() { return !suppressLaterPasses; };
-  auto addIfNeeded = [&](llvm::function_ref<bool()> predicate,
-                         llvm::function_ref<void()> passAdder) {
-    if (predicate())
-      passAdder();
-  };
-
-  auto addIRLevel = [&](int level, llvm::function_ref<void()> passAdder) {
-    addIfNeeded(notSuppressed, [&]() {
-      // Add the pass if the input IR level is at least the current
-      // abstraction.
-      if (irInputLevel <= level)
-        passAdder();
-      // Suppresses later passes if we're emitting IR and the output IR level is
-      // the current level.
-      if (outputFormat == OutputIR && irOutputLevel == level)
-        suppressLaterPasses = true;
-    });
-  };
-
-  // Lower to only SCF abstractions
-  addIRLevel(IRLevel::PreCompile, [&]() {
-    /* Nothing to do */
-  });
-
-  // Lower to Calyx
-  addIRLevel(IRLevel::Core,
-             [&]() { pm.addPass(circt::createSCFToCalyxPass()); });
-
-  // Run Calyx transforms
-  addIRLevel(IRLevel::PostCompile, [&]() {
-    pm.addPass(createSimpleCanonicalizerPass());
-    // Eliminate Calyx's comb group abstraction
-    pm.addNestedPass<calyx::ComponentOp>(
-        circt::calyx::createRemoveCombGroupsPass());
-    pm.addPass(createSimpleCanonicalizerPass());
-
-    // Compile to FSM
-    pm.addNestedPass<calyx::ComponentOp>(circt::createCalyxToFSMPass());
-    pm.addPass(createSimpleCanonicalizerPass());
-    pm.addNestedPass<calyx::ComponentOp>(
-        circt::createMaterializeCalyxToFSMPass());
-    pm.addPass(createSimpleCanonicalizerPass());
-
-    // Eliminate Calyx's group abstraction
-    pm.addNestedPass<calyx::ComponentOp>(
-        circt::createRemoveGroupsFromFSMPass());
-    pm.addPass(createSimpleCanonicalizerPass());
-  });
-
-  // HW path.
-
-  addIRLevel(IRLevel::RTL, [&]() {
-    // Compile to HW
-    pm.addPass(circt::createCalyxToHWPass());
-    pm.addPass(createSimpleCanonicalizerPass());
-  });
-
-  addIRLevel(IRLevel::SV, [&]() {
-    pm.addPass(circt::createConvertFSMToSVPass());
-    loadHWLoweringPipeline(pm);
-  });
-
-  if (outputFormat == OutputVerilog) {
-    pm.addPass(createExportVerilogPass((*outputFile)->os()));
-  } else if (outputFormat == OutputSplitVerilog) {
-    pm.addPass(createExportSplitVerilogPass(outputFilename));
-  }
-
-  if (failed(pm.run(module)))
-    return failure();
-
-  if (outputFormat == OutputIR)
-    module->print((*outputFile)->os());
-
-  return success();
-}
 
 /// Process a single buffer of the input.
 static LogicalResult processBuffer(
@@ -477,23 +256,12 @@ static LogicalResult processBuffer(
   // Parse the input.
   mlir::OwningOpRef<mlir::ModuleOp> module;
   llvm::sys::TimePoint<> parseStartTime;
-  if (verbosePassExecutions) {
-    llvm::errs() << "[hlstool] Running MLIR parser\n";
-    parseStartTime = llvm::sys::TimePoint<>::clock::now();
-  }
+
   auto parserTimer = ts.nest("MLIR Parser");
   module = parseSourceFile<ModuleOp>(sourceMgr, &context);
 
   if (!module)
     return failure();
-
-  if (verbosePassExecutions) {
-    auto elpased = std::chrono::duration<double>(
-                       llvm::sys::TimePoint<>::clock::now() - parseStartTime) /
-                   std::chrono::seconds(1);
-    llvm::errs() << "[hlstool] -- Done in " << llvm::format("%.3f", elpased)
-                 << " sec\n";
-  }
 
   // Apply any pass manager command line options.
   PassManager pm(&context);
@@ -502,18 +270,8 @@ static LogicalResult processBuffer(
   if (failed(applyPassManagerCLOptions(pm)))
     return failure();
 
-  switch (hlsFlow) {
-  case HLSFlowDynamicHW: {
     if (failed(doHLSFlowDynamic(pm, module.get(), outputFile)))
       return failure();
-    break;
-  }
-  case HLSFlowCalyxHW: {
-    if (failed(doHLSFlowCalyx(pm, module.get(), outputFile)))
-      return failure();
-    break;
-  }
-  }
 
   // We intentionally "leak" the Module into the MLIRContext instead of
   // deallocating it.  There is no need to deallocate it right before process
@@ -560,8 +318,6 @@ processInput(MLIRContext &context, TimingScope &ts,
 }
 
 static LogicalResult executeHlstool(MLIRContext &context) {
-  if (allowUnregisteredDialects)
-    context.allowUnregisteredDialects();
 
   // Create the timing manager we use to sample execution times.
   DefaultTimingManager tm;
@@ -607,9 +363,6 @@ int main(int argc, char **argv) {
   // llvm/circt and not llvm/llvm-project.
   setBugReportMsg(circtBugReportMsg);
 
-  // Hide default LLVM options, other than for this tool.
-  // MLIR options are added below.
-  cl::HideUnrelatedOptions(mainCategory);
 
   // Register any pass manager command line options.
   registerMLIRContextCLOptions();
