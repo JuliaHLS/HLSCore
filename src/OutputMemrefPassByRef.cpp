d89e5741575d82875f910a76a0236e2dbe251893
Benedict Short <benedictnjshort@gmail.com>
2025-03-08 18:05:41 +0000

    Added (empty) memref to pass by reference pass

---
+++ src/OutputMemrefPassByRef.cpp
@@ -0,0 +1,6 @@
+#include "OutputMemrefPassByRef.h"
+
+
+/* std::unique_ptr<mlir::Pass> HLSPasses::createOutputMemrefPassByRef() { */
+/*     return std::make_unique<HLSPasses::OutputMemrefPassByRef>(); */
+/* } */
