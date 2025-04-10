2c698851a11852a877f691178a4faf6e0d86a01f
Benedict Short <benedictnjshort@gmail.com>
2025-04-10 17:58:14 +0100

    Tidying Static Pipeline Imports

--- include/HLSStatic.hpp
+++ include/HLSStatic.hpp
@@ -1,69 +1,17 @@
 #pragma once
 
 
-#include <iostream>
-#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
-#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
-#include "mlir/Conversion/TosaToLinalg/TosaToLinalg.h"
-#include "mlir/Dialect/ControlFlow/Transforms/BufferizableOpInterfaceImpl.h"
-
+// MLIR Imports
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tensor/TransformOps/TensorTransformOps.h"
-#include "mlir/Dialect/Tensor/Transforms/BufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/Linalg/Transforms/AllInterfaces.h"
 #include "mlir/Dialect/Linalg/Transforms/Transforms.h"
 #include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/TosaToLinalg/TosaToLinalg.h"
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
 
-
-#include "mlir/Dialect/Linalg/IR/Linalg.h"
-#include "mlir/Dialect/Arith/Transforms/BufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/ControlFlow/Transforms/BufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
-#include "mlir/Dialect/Bufferization/Transforms/Transforms.h"
-#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
-#include "mlir/Dialect/Bufferization/Transforms/FuncBufferizableOpInterfaceImpl.h"
-
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/IR/AsmState.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/Parser/Parser.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassInstrumentation.h"
-#include "mlir/Pass/PassManager.h"
-#include "mlir/Support/FileUtilities.h"
-#include "mlir/Support/Timing.h"
-#include "mlir/Support/ToolUtilities.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "mlir/Transforms/Passes.h"
-
-#include "llvm/Support/Chrono.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/FileSystem.h"
-#include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/Path.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/ToolOutputFile.h"
-
-#include "circt/Conversion/ExportVerilog.h"
-#include "circt/Conversion/Passes.h"
-#include "circt/Dialect/DC/DCPasses.h"
-#include "circt/Dialect/ESI/ESIDialect.h"
-#include "circt/Dialect/ESI/ESIPasses.h"
-#include "circt/Dialect/SV/SVDialect.h"
-#include "circt/Dialect/SV/SVPasses.h"
-#include "circt/Dialect/Seq/SeqDialect.h"
-#include "circt/Dialect/Seq/SeqPasses.h"
-#include "circt/Support/LoweringOptions.h"
-#include "circt/Support/LoweringOptionsParser.h"
-#include "circt/Support/Version.h"
-#include "circt/Transforms/Passes.h"
-
+// calyx imports
 #include "circt/Conversion/CalyxToFSM.h"
 #include "circt/Conversion/SCFToCalyx.h"
 #include "circt/Dialect/Calyx/CalyxDialect.h"
@@ -73,14 +21,26 @@
 #include <circt/Conversion/SCFToCalyx.h>
 #include <circt/Conversion/SeqToSV.h>
 
+// CIRCT Imports
+#include "circt/Conversion/ExportVerilog.h"
+#include "circt/Conversion/Passes.h"
+#include "circt/Dialect/SV/SVDialect.h"
+#include "circt/Dialect/SV/SVPasses.h"
+#include "circt/Dialect/Seq/SeqDialect.h"
+#include "circt/Dialect/Seq/SeqPasses.h"
+#include "circt/Support/LoweringOptions.h"
+#include "circt/Support/LoweringOptionsParser.h"
+#include "circt/Support/Version.h"
+#include "circt/Transforms/Passes.h"
 
-
+// HLSCore Imports
 #include "IRLevel.hpp"
 #include "Options.hpp"
 #include "CirctFriendlyLoops.hpp"
 #include "HLS.hpp"
 #include "OutputMemrefPassByRef.h"
 #include "logging.hpp"
+#include "LoweringPipelines.hpp"
 
 
 using namespace llvm;
@@ -94,6 +54,10 @@ class HLSToolStatic : public HLSTool {
 public:
     HLSToolStatic() {
         logging::runtime_log("Using Statically Scheduled HLS flow");
+
+        // register high-level dialects
+        HLSCore::pipelines::registerCoreDialects(registry);
+        HLSCore::pipelines::registerPreCompileDialects(registry);
 
         // register CIRCT dialects
         registry.insert<hw::HWDialect, comb::CombDialect, seq::SeqDialect,
